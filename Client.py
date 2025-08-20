5d390427b186ea96cbf6ee3e4d1400f37fc43b88
Dipak Waghmare <159625813+Dipak-w@users.noreply.github.com>
2025-08-20 15:54:00 +0530

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+ 
+# pip install openai 
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key="Your API Key Here",
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Assistant"},
+    {"role": "user", "content": "what is coding"}
+  ]
+)
+
+print(completion.choices[0]
+.message.content)
---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "NCM": "https://www.youtube.com/watch?v=5n2JPm7LZ6k1",
+    "Piano meditation": "https://www.youtube.com/watch?v=Rd0Ljh6gVZo",
+    "Soulful_Love": "https://youtu.be/yPxvsN4RI50?si=x2QAssp8yeHFY4Mb"
+}
